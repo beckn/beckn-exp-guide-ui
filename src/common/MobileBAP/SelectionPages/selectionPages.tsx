2829102db48f2aad5486fde8cfdce99bede15501
kundan <kdn.rthr@gmail.com>
2024-08-28 13:34:48 +0530

    chnages revert from prod

--- src/common/MobileBAP/SelectionPages/selectionPages.tsx
+++ src/common/MobileBAP/SelectionPages/selectionPages.tsx
@@ -17,10 +17,10 @@ const SelectionPages = () => {
   const retailAppUrl = process.env.REACT_APP_RETAIL_APP;
   const mobilityAppUrl = process.env.REACT_APP_MOBILITY_CITY_OF_LIGHT_URL;
 
-  // const handleMobilityLinkClick = (event: any) => {
-  //   event.preventDefault();
-  //   alert("This feature is under progress. Coming soon!");
-  // };
+  const handleMobilityLinkClick = (event: any) => {
+    event.preventDefault();
+    alert("This feature is under progress. Coming soon!");
+  };
 
   return (
     <div className="selectionPage_wrapper">
@@ -48,7 +48,7 @@ const SelectionPages = () => {
           href={`${mobilityAppUrl}?external_url=${orderObjectUrl}`}
           target=""
           rel="noopener noreferrer"
-          // onClick={handleMobilityLinkClick}
+          onClick={handleMobilityLinkClick}
         >
           <div className="Link_wrapper">
             <div className="link_img">
