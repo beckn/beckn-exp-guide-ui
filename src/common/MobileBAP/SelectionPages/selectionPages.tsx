df9e671a90a3a56e9bb22cde6ffdc05f70a32dc8
kundan <kdn.rthr@gmail.com>
2024-08-30 17:24:25 +0530

    fixed location issues in city of light for mobility and retail

--- src/common/MobileBAP/SelectionPages/selectionPages.tsx
+++ src/common/MobileBAP/SelectionPages/selectionPages.tsx
@@ -44,22 +44,19 @@ const SelectionPages = () => {
             <img src="/assets/arrow_forword_ios_24pxnavArrow.png" alt="" />
           </div>
         </a>
-        <div
-          className="Link_wrapper"
-          onClick={() => {
-            localStorage.setItem(
-              "mobility_external_url",
-              JSON.stringify(orderObjectUrl)
-            );
-            window.dispatchEvent(new Event("storage"));
-          }}
+        <a
+          href={`${mobilityAppUrl}/?external_url=${orderObjectUrl}`}
+          target=""
+          rel="noopener noreferrer"
         >
-          <div className="link_img">
-            <img src="/assets/bookRide.png" alt="" />
+          <div className="Link_wrapper">
+            <div className="link_img">
+              <img src="/assets/bookRide.png" alt="" />
+            </div>
+            <div style={{ fontSize: "20px" }}>book a ride</div>
+            <img src="/assets/arrow_forword_ios_24pxnavArrow.png" alt="" />
           </div>
-          <div style={{ fontSize: "20px" }}>book a ride</div>
-          <img src="/assets/arrow_forword_ios_24pxnavArrow.png" alt="" />
-        </div>
+        </a>
       </div>
     </div>
   );
--- src/common/Tabs/chooseExperience.tsx
+++ src/common/Tabs/chooseExperience.tsx
@@ -5932,7 +5932,7 @@ const ChooseExperience: React.FC<selectExpModalProps> = ({
                 //@ts-ignore
                 ref={iframeRef}
                 className="ChooseExpIframe"
-                allow="clipboard-read; clipboard-write; geolocation"
+                allow="clipboard-read; clipboard-write; geolocation *"
                 src={iframeURL}
                 frameBorder="0"
                 allowFullScreen
